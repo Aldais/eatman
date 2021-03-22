f483a0e7e462f0af0fb1f216676686c68736efa4
Aldais <fabien.lauroua@aldais.com>
2021-03-22 19:41:21 +0000

    [ADD] eatman: create product.py and add field

---
+++ eatman/models/.ipynb_checkpoints/product-checkpoint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+ class product(models.Model):
+    _name = 'eatman.product'
+    _description = 'eatman.product'
+
+    name = fields.Char()
+    product_sold = field.bool()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
