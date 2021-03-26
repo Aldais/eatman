34e94b24dbd87dd40849a2ffc2e48229140f3f81
Aldais <fabien.lauroua@aldais.com>
2021-03-26 15:26:52 +0000

    [ADD] eatman: décrire le commit

--- eatman/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ eatman/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,3 +2,4 @@
 
 from . import models
 from . import product
+from . import receipe
--- eatman/models/.ipynb_checkpoints/product-checkpoint.py
+++ eatman/models/.ipynb_checkpoints/product-checkpoint.py
@@ -4,11 +4,36 @@ from odoo import models, fields, api
 
 
 class product(models.Model):
-    _name = "eatman.product"
-    _description = "Product"
+    
+    _inherit = "product.template"
+    
+    product_cook = fields.Boolean(default=False,string="Produit cuisiné")
+    sale_ok = fields.Boolean(default=False,string="Produit vendu")
+    purchase_ok = fields.Boolean(default=False,string="Produit acheté")
+    
+    unit_of_reference = fields.Many2one('uom.uom', 'unité de référence',help="Unité de référence")
+    unit_of_cooking = fields.Many2one('uom.uom', 'unité de préparation',help="Unité de préparation")
+    unit_of_sale = fields.Many2one('uom.uom', 'unité de vente',help="Unité de vente")
+    unit_of_purchase = fields.Many2one('uom.uom', "unité d'achat" ,help="Unité d'achat")
+                                      
+    unit_of_inventory_1 = fields.Many2one('uom.uom', 'unité d inventaire1')
+    unit_of_inventory_2 = fields.Many2one('uom.uom', 'unité d inventaire2')
+    unit_of_inventory_3 = fields.Many2one('uom.uom', 'unité d inventaire3')
+    
+    conversion_sale = fields.Float(digits="3")
+    conversion_supply= fields.Float(digits="3")
+    conversion_cook= fields.Float(digits="3")
+    conversion_inventory1 = fields.Float(digits="3")
+    conversion_inventory2 = fields.Float(digits="3")
+    conversion_inventory3 = fields.Float(digits="3")
+    
+    ratio_sale = fields.Float(digits="3")
+    ratio_supply = fields.Float(digits="3")
+    ratio_cook = fields.Float(digits="3")
 
-    name = fields.Char()
-#     value = fields.Integer()
+    
+
+    #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
