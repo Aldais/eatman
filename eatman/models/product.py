7ffee78874ae9e65cb209e7355acdb6d9bbba936
Aldais <fabien.lauroua@aldais.com>
2021-03-26 17:14:15 +0000

    [ADD] eatman: correction Bug unité de mesure qui disparaissent

--- eatman/models/product.py
+++ eatman/models/product.py
@@ -11,39 +11,63 @@ class product(models.Model):
     sale_ok = fields.Boolean(default=False,string="Produit vendu")
     purchase_ok = fields.Boolean(default=False,string="Produit acheté")
     
-    unit_of_reference = fields.Many2one('uom.uom', 'unité de référence',help="Unité de référence")
-    unit_of_cooking = fields.Many2one('uom.uom', 'unité de préparation',help="Unité de préparation")
-    unit_of_sale = fields.Many2one('uom.uom', 'unité de vente',help="Unité de vente")
-    unit_of_purchase = fields.Many2one('uom.uom', "unité d'achat" ,help="Unité d'achat")
+    #Gestion des unités de mesure pour les différents process
+    #inventaire
+    #cuisine
+    #vente
+    #achat
+    #unité pivot de référence pour les 
                                       
     unit_of_inventory_1 = fields.Many2one('uom.uom', 'unité d inventaire1')
     unit_of_inventory_2 = fields.Many2one('uom.uom', 'unité d inventaire2')
     unit_of_inventory_3 = fields.Many2one('uom.uom', 'unité d inventaire3')
     
-    
+    unit_of_reference = fields.Many2one('uom.uom', 'unité de référence')
+    unit_of_cooking = fields.Many2one('uom.uom', 'unité de préparation')
+    unit_of_sale = fields.Many2one('uom.uom', 'unité de vente')
+    unit_of_purchase = fields.Many2one('uom.uom', "unité d'achat")
     
     #Conversion doit permettre de calculer facilement un ratio. Ex:
     #6 bouteilles = 1 pack
     #1 boite = 250 Grammes
     
-    conversion_sale_source = fields.Float(digits="3")
-    conversion_sale_target = fields.Float(digits="3")
+    conversion_sale_source_unit = fields.Char(related='unit_of_sale.name', store=True)
+    conversion_sale_target_unit = fields.Char(related='unit_of_reference.name', store=True)
     
-    conversion_purchase_source = fields.Float(digits="3")
-    conversion_purchase_target = fields.Float(digits="3")
+    conversion_purchase_source_unit = fields.Char(related='unit_of_purchase.name', store=True)
+    conversion_purchase_target_unit = fields.Char(related='unit_of_reference.name', store=True)
     
     
-    conversion_cook_source = fields.Float(digits="3")
-    conversion_cook_target = fields.Float(digits="3")
+    conversion_cook_source_unit = fields.Char(related='unit_of_cooking.name', store=True)
+    conversion_cook_target_unit = fields.Char(related='unit_of_reference.name', store=True)
+    
+    conversion_sale_source_quantity = fields.Float(digits="3")
+    conversion_sale_target_quantity = fields.Float(digits="3")
+    
+    conversion_purchase_source_quantity = fields.Float(digits="3")
+    conversion_purchase_target_quantity = fields.Float(digits="3")
+    
+    
+    conversion_cook_source_quantity = fields.Float(digits="3")
+    conversion_cook_target_quantity = fields.Float(digits="3")
     
     conversion_inventory1 = fields.Float(digits="3")
     conversion_inventory2 = fields.Float(digits="3")
     conversion_inventory3 = fields.Float(digits="3")
     
-    ratio_sale = fields.Float(digits="3")
-    ratio_supply = fields.Float(digits="3")
-    ratio_cook = fields.Float(digits="3")
+    ratio_sale = fields.Float(compute="_value_ratio_sale", store=True, digits="3")
+    
+    @api.depends('conversion_sale_source_quantity','conversion_sale_target_quantity')
+    def _value_ratio_sale(self):
+        for record in self:
+            record.ratio_sale = float(record.conversion_sale_source_quantity) / float(record.conversion_sale_target_quantity)
+    
+    
+    
+    #ratio_supply = fields.Float(compute="_value_ratio_supply", store=True, digits="3")
+    #ratio_cook = fields.Float(compute="_value_ratio_cook", store=True, digits="3")
 
+     
     
 
     #     value = fields.Integer()
